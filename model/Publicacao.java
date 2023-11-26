24198b8b1f34c97ed34ebfc19c31aa40925fb03f
JuliaLilo <133895910+JuliaLilo@users.noreply.github.com>
2023-11-26 20:13:22 -0300

    Update Publicacao.java

--- model/Publicacao.java
+++ model/Publicacao.java
@@ -1,11 +1,15 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Publicacao {
     private long idPublicacao;
     private String imagem;
     private String video;
     private String documento;
-    private String comentario;
+    private List<Comentario> listComentario;
+    private List<Chapa> listChapa;
 
     public Publicacao (){
         super();
@@ -13,46 +17,71 @@ public class Publicacao {
         this.imagem = " ";
         this.video = " ";
         this.documento = " ";
-        this.comentario = " ";
-    }
+        this.listComentario = new ArrayList<>();
+        this.listChapa = new ArrayList<>();
+        }
 
-    public Publicacao (long idPublicacao, String imagem, String video, String documento, String comentario) {
+    public Publicacao (long idPublicacao, String imagem, String video, String documento, List<Comentario> 
+    listComentario, List<Chapa> listChapa) {
         super();
         this.idPublicacao = idPublicacao;
         this.imagem = imagem;
         this.video = video;
         this.documento = documento;
-        this.comentario = comentario;
+        this.listComentario = listComentario;
+        this.listChapa = listChapa;
     }
 
     public long getIdPublicacao (){
         return idPublicacao;
     }
+
     public void setIdPublicacao (long idPublicacao){
         this.idPublicacao = idPublicacao;
     }
+
     public String getImagem (){
         return imagem;
     }
+
     public void setImagem (String imagem){
         this.imagem = imagem;
     }
-    public String getVideo (){
+
+    public String getVideo(){
         return video;
     }
-    public void setVideo (String video){
+
+    public void setVideo(String video){
         this.video = video;
     }
-    public String getDocumento (){
+
+    public String getDocumento(){
         return documento;
     }
-    public void setDocumento (String documento){
+
+    public void setDocumento(String documento){
         this.documento = documento;
     } 
-    public String getComentario (){
-        return comentario;
+
+    public List<Comentario> getListComentario() {
+        return listComentario;
     }
-    public void setComentario (String comentario){
-        this.comentario = comentario;
+
+    public void setListComentario(List<Comentario> listComentario){
+        this.listComentario = listComentario;
+    }
+
+    public List<Chapa> getListChapa(){
+        return listChapa;
+    }
+
+    public void setListChapa (List<Chapa> listChapa){
+        this.listChapa = listChapa;
+    }
+
+    @Override
+    public String toString(){
+        return idPublicacao+ ", " +imagem+ ", " +video+ ", " +documento+ ", " +listComentario+ ", " +listChapa;
     }
 }
