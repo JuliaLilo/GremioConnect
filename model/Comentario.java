fbe9afbfd8a4f777e896e63dfb013b006161f203
JuliaLilo <133895910+JuliaLilo@users.noreply.github.com>
2023-11-26 20:09:15 -0300

    Update Comentario.java

--- model/Comentario.java
+++ model/Comentario.java
@@ -2,15 +2,21 @@ package model;
 
 public class Comentario {
     private long idComentario;
+    private Estudante estudante;
+    private Publicacao publicacao;
 
     public Comentario (){
         super();
         this.idComentario = 0;
+        this.publicacao = new Publicacao();
+        this.estudante = new Estudante();
     }
 
-    public Comentario (long idComentario){
+    public Comentario (long idComentario, Estudante estudante, Publicacao Publicacao){
         super();
         this.idComentario = idComentario;
+        this.estudante = estudante;
+        this.publicacao = publicacao;
     }
 
     public long getIdComentario() {
@@ -20,5 +26,27 @@ public class Comentario {
     public void setIdComentario(long idComentario) {
         this.idComentario = idComentario;
     }
-    
+
+    public Publicacao getPublicacao() {
+        return publicacao;
+    }
+
+    public void setPublicacao (Publicacao publicacao){
+        this.publicacao = publicacao;
+    }
+
+    public Estudante getEstudante (){
+        return estudante;
+    }
+
+    public void setestudante (Estudante estudante){
+        this.estudante = estudante;
+    }
+
+    @Override
+    public String toString(){
+        return idComentario+ ", " +publicacao+ ", " +estudante;
+    }
+
 }
+
