f07c7cbae287ed8de01fa607b824c207e19b1b71
JuliaLilo <133895910+JuliaLilo@users.noreply.github.com>
2023-11-26 20:04:22 -0300

    Update Chat.java

--- model/Chat.java
+++ model/Chat.java
@@ -1,14 +1,20 @@
 package model;
 public class Chat {
     private long idChat;
+    private Chapa chapa;
+    private Estudante estudante;
 
-    public Chat(long idChat){
+    public Chat(long idChat, Chapa chapa, Estudante estudante){
     super();
     this.idChat = idChat;
+    this.chapa = chapa;
+    this.estudante = estudante;
 }
     public Chat(){
     super();
     this.idChat = 0;
+    this.chapa = new Chapa();
+    this.estudante = new Estudante();
     }
 
     public long getIdChat() {
@@ -17,5 +23,30 @@ public class Chat {
 
     public void setIdChat(long idChat) {
         this.idChat = idChat;
+    }
+
+    public Chapa getChapa (){
+        return chapa;
+    }
+
+    public void setChapa (Chapa chapa){
+        this.chapa = chapa;
+    }
+
+    public Estudante getEstudante (){
+        return estudante;
+    }
+
+    public void setEstudante (Estudante estudante){
+        this.estudante = estudante;
+    }
+
+    @Override
+    public String toString(){
+        return idChat+ ", " +chapa+ ", " +estudante;
+    }
+    public void setChapa(String string) {
+    }
+    public void setEstudante(String string) {
     }
 }
