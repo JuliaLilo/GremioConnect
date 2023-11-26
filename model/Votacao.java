a3de38db75b45a9da45009d205f8fbc46371dfd7
JuliaLilo <133895910+JuliaLilo@users.noreply.github.com>
2023-11-26 20:13:57 -0300

    Update Votacao.java

--- model/Votacao.java
+++ model/Votacao.java
@@ -2,18 +2,22 @@ package model;
 
 public class Votacao {
     private long idVotacao;
-
+    private Chapa chapa;
+    private Estudante estudante;
     
     public Votacao (){
         super();
         this.idVotacao = 0;
+        this.chapa = new Chapa();
+        this.estudante = new Estudante();
     }
 
-    public Votacao (long idVotacao){
+    public Votacao (long idVotacao, Estudante estudante, Chapa chapa){
         super();
         this.idVotacao = idVotacao;
+        this.estudante = estudante;
+        this.chapa = chapa;
     }
-
 
     public long getIdVotacao() {
         return idVotacao;
@@ -22,4 +26,25 @@ public class Votacao {
     public void setIdVotacao(long idVotacao) {
         this.idVotacao = idVotacao;
     }
+
+    public Estudante getEstudante (){
+        return estudante;
+    }
+
+    public void setEstudante (Estudante estudante){
+        this.estudante = estudante;
+    }
+
+    public Chapa getChapa(){
+        return chapa;
+    }
+
+    public void setChapa(Chapa chapa){
+        this.chapa = chapa; 
+    }
+
+    @Override
+    public String toString(){
+        return idVotacao+ ", " +chapa+ ", " +estudante;
+    }    
 }
