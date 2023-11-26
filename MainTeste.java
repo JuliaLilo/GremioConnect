c7f1dd091e9ed1d7f3ca49f2688066a806cfee22
JuliaLilo <133895910+JuliaLilo@users.noreply.github.com>
2023-11-26 20:15:37 -0300

    Update and rename MainTeste.java to Tela1.java

--- MainTeste.java
+++
@@ -1,8 +0,0 @@
-import persistencia.Conexao2;
-
-public class MainTeste {
-    public static void main (String args []){
-        Conexao2 ds = new Conexao2();
-        ds.closeDataSource();
-    }
-}
---
+++ Tela1.java
@@ -0,0 +1,69 @@
+package telas;
+
+import model.Chapa;
+import model.Estudante;
+import persistencia.ChapaDAO;
+import persistencia.EstudanteDAO;
+import java.util.List;
+
+public class Tela1 {
+    public static void main(String[] args) {
+    
+
+     // =================================ESTUDANTE============================
+        //ADICIONAR
+   // Estudante estudante = new Estudante(0, "juliapiresineia@gmail.com", "Julia Lilo", 
+   // "123", null, null, null, null, null);
+   // EstudanteDAO estudanteDAO = new EstudanteDAO();
+   // estudanteDAO.adicionar(estudante);
+
+        //EDITAR
+   // Estudante estudante = new Estudante(1, "juliapiresineia@gmail.com", "Julia Lilo Pires Ineia", 
+   // "321", null, null, null, null, null);
+   // EstudanteDAO estudanteDAO = new EstudanteDAO();
+   // estudanteDAO.editar(estudante)
+
+        //EXCLUIR
+   // Estudante estudante = new Estudante();
+   // estudanteDAO.excluir(1);
+
+        //BUSCAR POR ID
+   // EstudanteDAO estudanteDAO = new EstudanteDAO();
+   // Estudante estudante = estudanteDAO.buscarporId(2);
+   // System.out.println(Estudante);
+
+   //BUSCAR TODOS
+   // EstudanteDAO estudanteDAO = new EstudanteDAO();
+   // List<Estudante> listaEstudantes = estudanteDAO.buscarTodos();
+   // System.out.println(listaEstudantes.size());
+
+   //===============================CHAPA================
+
+          //ADICIONAR
+   // Chapa chapa = new Chapa(0, "456", "Credo", 
+   // "Nome", null, null, null, null, null, null);
+   // ChapaDAO chapaDAO = new ChapaDAO();
+   // chapaDAO.adicionar(chapa);
+
+        //EDITAR
+   // Chapa chapa = new Chapa(0, "456", "Credo", 
+   // "Nome", null, null, null, null, null, null);
+   // EstudanteDAO estudanteDAO = new EstudanteDAO();
+   // estudanteDAO.editar(estudante)
+
+        //EXCLUIR
+   // Estudante estudante = new Estudante();
+   // estudanteDAO.excluir(1);
+
+        //BUSCAR POR ID
+   // EstudanteDAO estudanteDAO = new EstudanteDAO();
+   // Estudante estudante = estudanteDAO.buscarporId(2);
+   // System.out.println(Estudante);
+
+   //BUSCAR TODOS
+   // EstudanteDAO estudanteDAO = new EstudanteDAO();
+   // List<Estudante> listaEstudantes = estudanteDAO.buscarTodos();
+   // System.out.println(listaEstudantes.size());
+}
+}
+
